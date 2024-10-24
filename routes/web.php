4862e75a7de26c1bc6317b87237e9d8c9f15f98b
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-10-24 18:21:09 +0200

    create a controllore for home page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.home');
-});
+Route::get('/',[ PageController::class,"home"]);
 
-Route::get('/',[ MovieController::class,"index"]);
+Route::get('/movies',[ MovieController::class,"index"]);
